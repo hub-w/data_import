2ca9cfb5ffebc9b8fddb0a8984329341e9744c30
Geg <it_geg@163.com>
2019-11-18 18:44:24 +0800

    多线程处理

--- src/main/java/cn/ac/iie/dptmq/SuccessHandler.java
+++ src/main/java/cn/ac/iie/dptmq/SuccessHandler.java
@@ -23,10 +23,7 @@ import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
     private static final Logger logger = Logger.getLogger(SuccessHandler.class);
@@ -50,13 +47,15 @@ public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
             .decoder(new JacksonDecoder())
             .target(SynthesizeService.class, CommonConstant.get("synthesize_url"));
 
-
-
+           static Request<NewsComment> request = new Request<>();
+           static int count = 0;
     @Override
     public boolean handle(REMessage message) {
         long handleStart = System.currentTimeMillis();
         logger.info("handle开始：：：获取数据：" + message.data.size() + "条");
-
+        //总数据
+        List<Object[]> messageList = message.data;
+        List<Object[]> messageList1 = new ArrayList<>();
         try {
             logger.info("数据获取成功" + message.data.size());
             //如果colNameIndex为空，则遍历colNames，建立字段名和索引之间的映射
@@ -71,175 +70,36 @@ public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
 
             //获取数据并存入Map
             //遍历数据集合，获得每条数据的数组
-            Request<NewsComment> request = new Request<>();
-            int count = 0;
 
-            for (Object[] data : message.data) {
-                //logger.info("data::::::::::::"+data);
-                long allStart = System.currentTimeMillis();
-                count++;
-
-//                if (count % Integer.parseInt(CommonConstant.get("deal_num")) == 0) {
-
-                    // 读取数据
-                    String[] colNames = message.colNames;
-                    logger.info(Arrays.deepToString(colNames));
-                    Map<String, Object> propertiesMap = new HashMap<>();
-                    for (String colName : colNames) {
-                        String col = CommonConstant.get(colName);
-                        if(col != null) {
-                            Object value = null;
-                            if(colName.endsWith("time")) {
-                                value = new Date((Long) data[colNameIndex.get(colName)] * 1000);
-                            } else {
-                                value = data[colNameIndex.get(colName)];
-                            }
-                            if(colName.equals("m_risk_label")){
-                                value = null;
-                            }
-                            propertiesMap.put(col, value);
-                        }
-                    }
-
-                    //logger.info("数据：：："+propertiesMap);
-                    // ==============================================================================
-                    
-                    NewsComment temp = new NewsComment();
-                    BeanUtils.populate(temp, propertiesMap);
-
-
-                    logger.info("格式"+temp);
-
-                    //下载
-                    /*Integer xiazaicount=0;
-                    String[] videoFiles= temp.getmVideoFiles();
-                    String url = "http://10.136.134.35:20099/get?key="+videoFiles[0];
-                    logger.info("下载地址"+url);
-                    String tmpDir = "/home/iie/videodownload/" + "video/"  + "douyin/";
-                    File folder = new File(tmpDir);
-                    if (!folder.exists()) {
-                        folder.mkdirs();
-                    }
-                    String filePath = tmpDir + File.separator +url.substring(url.lastIndexOf("@") + 1);
-                    File f = new File(filePath);
-                    try {
-                        FileUtils.downloadFile(url, f);
-                        logger.info("下载成功");
-                    } catch (Exception e) {
-                        logger.info(e.toString());
-                        logger.info("下载失败");
-                    }*/
-
-                    //request.addData(temp);//不过引擎直接入库
-
-                    String contentId = (String) propertiesMap.get("gChKey");
-                    String content = (String) propertiesMap.get("mContent");
-                    if(StringUtils.isBlank(content)){
-                        content = (String) propertiesMap.get("mTitle");
-                    }
-
-                    // 1 filter hitblack
-                    FilterRequest filterRequest = new FilterRequest();
-                    // 构建过滤请求参数
-                    filterRequest.setSysCode(CommonConstant.get("filter_sys_code"));
-                    filterRequest.setId(contentId);
-                    filterRequest.setText(content);
-
-                    logger.info("过滤请求参数：：："+new ObjectMapper().writeValueAsString(filterRequest));
-                    FilterResponse filterResponse = filterService.filter(filterRequest);
-                    String filterResponseJson = new ObjectMapper().writeValueAsString(filterResponse);
-                    logger.info("过滤返回：：："+filterResponseJson);
-                    if(filterResponse.isStatus()) {
-                        FilterRes filterRes = filterResponse.getFilterRes();
-                        if(filterRes.getIsPush()) {
-                            // 2、综合引擎
-//                            SynthesizeRequest synthesizeRequest = new SynthesizeRequest();
-//                            synthesizeRequest.setuChId(temp.getuChId());
-//                            synthesizeRequest.setmChId(temp.getmChId());
-//                            synthesizeRequest.setuName(temp.getuName());
-//                            synthesizeRequest.setmContent(content);
-//                            synthesizeRequest.setmPublishTime(sdf.format(temp.getmPublishTime()));
-//                            synthesizeRequest.setgAsp(temp.getgAsp());
-                            JSONObject synthesizeRequest = new JSONObject();
-                            JSONArray datas = new JSONArray();
-                            JSONObject json= new JSONObject();
-                            json.put("m_content",temp.getmContent());
-                            json.put("m_publish_time",sdf.format(temp.getmPublishTime()));
-                            json.put("u_name",temp.getuName());
-                            json.put("u_ch_id",temp.getuChId());
-                            json.put("m_ch_id",temp.getmChId());
-                            json.put("g_asp",temp.getgAsp());
-                            datas.add(json);
-                            synthesizeRequest.put("data",datas);
-//                            logger.info("综合引擎请求参数:::"+synthesizeRequest);
-//                            //SynthesizeResponse synthesizeResponse = synthesizeService.synthesize(synthesizeRequest);
-//                            JSONObject synthesizeResponse = synthesizeService.synthesize(synthesizeRequest);
-//                            logger.info("综合引擎返回：：："+synthesizeResponse);
-//                            JSONArray redatas = synthesizeResponse.getJSONArray("data");
-//                            JSONObject  redata = redatas.getJSONObject(0);
-//                            String suggest = redata.getString("suggest");
-//
-//                            if("Harm".equalsIgnoreCase(suggest)) {
-                                //if(true) {
-                                logger.info("数据判定有害");
-
-                                // 构建数据
-                                List<Result> results = filterRes.getDescs();
-                                logger.info("results:"+results);
-                                int a=0;
-                                List<String> maxSubjectIds = new ArrayList<>();
-                                for (int i = 0; i < results.size(); i++) {
-                                    Result result=results.get(i);
-                                    String hitBlack = result.getHitBlack();
-                                    List<String> subjectIds = result.getSubjectIds();
-                                    int b= subjectIds.size();
-                                    if(b>a){
-                                        a=b;
-                                        maxSubjectIds = subjectIds;
-                                    }
-                                    if(i==(results.size()-1)){
-                                        for (String subjectId : maxSubjectIds) {
-                                            //只存第一个关键词和
-                                            //String subjectId=subjectIds.get(0);
-                                            NewsComment newsComment = new NewsComment();
-                                            BeanUtils.populate(newsComment, propertiesMap);
-                                            newsComment.setHitKeyword(hitBlack);
-                                            newsComment.setTopicId(subjectId);
-
-                                            if(content.length() < 150) {
-                                                newsComment.setmAbstract(content);
-                                            } else {
-                                                newsComment.setmAbstract(result.getHitAbstract());
-                                            }
-
-
-
-                                            logger.info("构建后数据为：：："+newsComment);
-
-                                            request.addData(newsComment);
-                                        }
-                                    }
-                                }
-//                            }
-                        }
-                    }
-//                } else {
-//                    logger.info("丢弃数据" + data[colNameIndex.get("g_ch_key")]);
-//                }
-
-                logger.info(data[colNameIndex.get("g_ch_key")] + ":::数据总耗时：：：" + (System.currentTimeMillis() - allStart));
-
+           /* //总条数
+            int size = messageList.size();
+            //获取结果中的一部分
+            int dataNum =Integer.parseInt(CommonConstant.get("dataNum"));
+            int num = size / dataNum;
+            //以总条数为范围,随机生成一部分的条数索引,用于监测随机评论
+            int[] messageIndex = getRandomArrayByIndex(num,size);
+            Object[] objects = null;
+            for (int i = 0; i < messageIndex.length; i++) {
+                objects = messageList.get(i);
+                messageList1.add(objects);
+            }
+            logger.info("处理的部分数据：" + messageList1.size() + "条");*/
+//            ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
+//            ExecutorService pool = Executors.newFixedThreadPool(10);// 创建一个固定大小为5的线程池
+            ExecutorService threadPool = ThreadPool.getThreadPool();
+           for (Object[] data : message.data) {
+               threadPool.execute(new SuccessHandlerThread(data,message,colNameIndex,logger,filterService,sdf));
             }
 
-            if (request.getData()!=null) {
-                logger.info("调用插入接口：：："+new ObjectMapper().writeValueAsString(request));
-                Response response=new Response();
-                if(CommonConstant.get("dpt_mq_topic").contains("comment")){
+            if (request.getData() != null) {
+                logger.info("调用插入接口：：：" + new ObjectMapper().writeValueAsString(request));
+                Response response = new Response();
+                if (CommonConstant.get("dpt_mq_topic").contains("comment")) {
                     response = insertService.insertNewsComment(request);
-                }else{
+                } else {
                     response = insertVideoService.insertNewsComment(request);
                 }
-                logger.info("插入结果：：：："+response);
+                logger.info("插入结果：：：：" + response);
 
                 logger.info("数据处理成功：" + message.data.size() + "条");
             }
@@ -253,14 +113,36 @@ public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
         return true;
     }
 
+    public static int[] getRandomArrayByIndex(int num, int scope) {
+        //1.获取scope范围内的所有数值，并存到数组中
+        int[] randomArray = new int[scope];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = i;
+        }
+
+        //2.从数组random中取数据，取过后的数改为-1
+        int[] numArray = new int[num];//存储num个随机数
+        int i = 0;
+        while (i < numArray.length) {
+            int index = (int) (Math.random() * scope);
+            if (randomArray[index] != -1) {
+                numArray[i] = randomArray[index];
+                randomArray[index] = -1;
+                i++;
+            }
+        }
+
+        return numArray;
+    }
+
     public static void main(String[] args) {
-        String aaa= "";
+        String aaa = "";
         parse();
         Map<String, Object> propertiesMap = new HashMap<>();
-        List<String> a=new ArrayList<>();
+        List<String> a = new ArrayList<>();
         a.add("1");
         a.add("2");
-        propertiesMap.put("mTags",a);
+        propertiesMap.put("mTags", a);
         logger.info(propertiesMap);
         NewsComment temp = new NewsComment();
         try {
@@ -278,30 +160,30 @@ public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
         Result result = new Result();
         result.setHitBlack("主席");
         result.setHitAbstract("毛主席逝世43周年");
-        List<String> ruids=new ArrayList<>(Arrays.asList("wrafdsgagag"));
+        List<String> ruids = new ArrayList<>(Arrays.asList("wrafdsgagag"));
         result.setRuleIds(ruids);
-        List<String> subids=new ArrayList<>(Arrays.asList("fgsggagaaa"));
+        List<String> subids = new ArrayList<>(Arrays.asList("fgsggagaaa"));
         result.setSubjectIds(subids);
         results.add(result);
-        logger.info("results:"+results);
-        int a=0;
+        logger.info("results:" + results);
+        int a = 0;
         List<String> maxSubjectIds = new ArrayList<>();
         for (int i = 0; i < results.size(); i++) {
-            Result re=results.get(i);
+            Result re = results.get(i);
             String hitBlack = re.getHitBlack();
             List<String> subjectIds = re.getSubjectIds();
-            int b= subjectIds.size();
-            if(b>a){
-                a=b;
+            int b = subjectIds.size();
+            if (b > a) {
+                a = b;
                 maxSubjectIds = subjectIds;
             }
             System.out.println();
-            if(i==(results.size()-1)){
+            if (i == (results.size() - 1)) {
                 for (String subjectId : maxSubjectIds) {
                     System.out.println(subjectId);
                 }
-                    //只存第一个关键词和
-                    //String subjectId=subjectIds.get(0);
+                //只存第一个关键词和
+                //String subjectId=subjectIds.get(0);
 //                    NewsComment newsComment = new NewsComment();
 //                    BeanUtils.populate(newsComment, propertiesMap);
 //                    newsComment.setHitKeyword(hitBlack);
@@ -318,10 +200,9 @@ public class SuccessHandler extends REAbstractReceiveMessageHandler<REMessage> {
 //                    logger.info("构建后数据为：：："+newsComment);
 //
 //                    request.addData(newsComment);
-                }
             }
         }
-
+    }
 
 }
 
